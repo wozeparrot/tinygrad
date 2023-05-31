03b4aea0576281d5a88af8350ba301722ed6154d
Woze Parrot <wozeparrot@gmail.com>
2023-05-31 16:24:30 +0000

    clean: formatting

--- models/maskrcnn.py
+++ models/maskrcnn.py
@@ -80,38 +80,38 @@ def _generate_anchors(base_size, scales, aspect_ratios):
   return anchors
 
 def _whctrs(anchor):
-    w = anchor[2] - anchor[0] + 1
-    h = anchor[3] - anchor[1] + 1
-    x_ctr = anchor[0] + 0.5 * (w - 1)
-    y_ctr = anchor[1] + 0.5 * (h - 1)
-    return w, h, x_ctr, y_ctr
+  w = anchor[2] - anchor[0] + 1
+  h = anchor[3] - anchor[1] + 1
+  x_ctr = anchor[0] + 0.5 * (w - 1)
+  y_ctr = anchor[1] + 0.5 * (h - 1)
+  return w, h, x_ctr, y_ctr
 
 def _mkanchors(ws, hs, x_ctr, y_ctr):
-    ws = ws[:, np.newaxis]
-    hs = hs[:, np.newaxis]
-    anchors = np.hstack((
-      x_ctr - 0.5 * (ws - 1),
-      y_ctr - 0.5 * (hs - 1),
-      x_ctr + 0.5 * (ws - 1),
-      y_ctr + 0.5 * (hs - 1),
-    ))
-    return anchors
+  ws = ws[:, np.newaxis]
+  hs = hs[:, np.newaxis]
+  anchors = np.hstack((
+    x_ctr - 0.5 * (ws - 1),
+    y_ctr - 0.5 * (hs - 1),
+    x_ctr + 0.5 * (ws - 1),
+    y_ctr + 0.5 * (hs - 1),
+  ))
+  return anchors
 
 def _ratio_enum(anchor, ratios):
-    w, h, x_ctr, y_ctr = _whctrs(anchor)
-    size = w * h
-    size_ratios = size / ratios
-    ws = np.round(np.sqrt(size_ratios))
-    hs = np.round(ws * ratios)
-    anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
-    return anchors
+  w, h, x_ctr, y_ctr = _whctrs(anchor)
+  size = w * h
+  size_ratios = size / ratios
+  ws = np.round(np.sqrt(size_ratios))
+  hs = np.round(ws * ratios)
+  anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
+  return anchors
 
 def _scale_enum(anchor, scales):
-    w, h, x_ctr, y_ctr = _whctrs(anchor)
-    ws = w * scales
-    hs = h * scales
-    anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
-    return anchors
+  w, h, x_ctr, y_ctr = _whctrs(anchor)
+  ws = w * scales
+  hs = h * scales
+  anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
+  return anchors
 
 class RPNHead:
   def __init__(self, in_channels, num_anchors):
