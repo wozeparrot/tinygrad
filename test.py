0348841b7de7887ab0feba7259f6f578e5bcaa2a
Woze Parrot <wozeparrot@gmail.com>
2024-05-21 22:32:47 -0700

    fix: allow beaming

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 from tinygrad import Tensor
 import numpy as np
 
-t = Tensor.randn(4096, 4096).shard(("HSA:0", "REMOTE:10.0.0.10:1234:CUDA:0"), axis=-1)
-t2 = Tensor.randn(4096, 4096).shard(("HSA:0", "REMOTE:10.0.0.10:1234:CUDA:0"), axis=None)
-t3 = t @ t2
+if __name__ == "__main__":
+  t = Tensor.randn(4096, 4096).shard(("HSA:0", "REMOTE:10.0.0.10:1234:CUDA:0"), axis=-1)
+  t2 = Tensor.randn(4096, 4096).shard(("HSA:0", "REMOTE:10.0.0.10:1234:CUDA:0"), axis=None)
+  t3 = t @ t2
 
-t3_np = t.numpy() @ t2.numpy()
-np.testing.assert_allclose(t3.numpy(), t3_np, atol=1e-3)
+  t3_np = t.numpy() @ t2.numpy()
+  np.testing.assert_allclose(t3.numpy(), t3_np, atol=1e-3)
