bf62621f194c5460e51f7a38b25ed7ef4c692a2d
Woze Parrot <wozeparrot@gmail.com>
2026-01-30 06:41:32 +0000

    feat: better gpuburn

--- examples/tools/gpuburn.py
+++ examples/tools/gpuburn.py
@@ -1,16 +1,25 @@
+import time
 from tinygrad import Tensor, Device, TinyJit, dtypes
-from tinygrad.helpers import getenv
+from tinygrad.helpers import GlobalCounters, getenv
 
 GPUS = getenv("GPUS", 4) # TODO: expose a way in tinygrad to access this
-N = 6144
+N = getenv("N", 6144)
+ROUNDS = getenv("ROUND", 8)
 
 @TinyJit
 def many_matmul(A, B):
   out = A
-  for _ in range(8): out = out@B
+  for _ in range(ROUNDS): out = out@B
   return out
 
 if __name__ == "__main__":
   A = Tensor.ones(GPUS, N, N, dtype=dtypes.half).shard(devices=tuple([f"{Device.DEFAULT}:{i}" for i in range(GPUS)]), axis=0).contiguous()
   B = Tensor.ones(GPUS, N, N, dtype=dtypes.half).shard(devices=tuple([f"{Device.DEFAULT}:{i}" for i in range(GPUS)]), axis=0).contiguous()
-  while 1: many_matmul(A, B)
+  while 1:
+    for _ in range(10):
+      GlobalCounters.reset()
+      st = time.perf_counter()
+      many_matmul(A, B)
+      et = time.perf_counter()
+    gflops = GlobalCounters.global_ops / (et - st) / 1e9
+    print(f"{gflops:.2f} GFLOPS")
